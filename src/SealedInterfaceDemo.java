04a3fee58e253cd0f1576d69f2669501882e2ce3
eddiedtran <56004674+eddietran@users.noreply.github.com>
2025-10-02 21:20:19 -0400

    Record demo

--- src/SealedInterfaceDemo.java
+++ src/SealedInterfaceDemo.java
@@ -21,18 +21,27 @@ class Salmon extends Fish {
 	
 }
 
+class Parrot {
+	
+}
+
 public class SealedInterfaceDemo {
 	
 	public static void main(String[] args) {
+		
 		speak(new Dog());
 		speak(new Cat());
+		speak(new Tabby());
 		speak(new Salmon());
 		speak(new Fish());
+		speak(new Parrot()); // java.lang.IllegalArgumentException: Not permitted animal
 	}
 	
-	public static void speak(Animal animal) {
+	public static void speak(Object animal) {
+		// Case for object of child class must precede case for object of parent class
 		switch (animal) {
 			case Dog d -> System.out.println("Woof");
+			case Tabby t -> System.out.println("Meow!");
 			case Cat c -> System.out.println("Meow");
 			case Salmon s -> System.out.println("I do not speak"); 
 			case Fish f -> System.out.println("I cannot speak");
